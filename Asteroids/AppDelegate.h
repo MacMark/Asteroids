4bcf13e16b2ff4d6af970c78a4a658b54adaf30f
Markus MoÃàller <macmark@macmark.de>
2015-09-17 17:59:08 +0200

    Initial Commit

---
+++ Asteroids/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Asteroids
+//
+//  Created by Markus Möller on 17/09/15.
+//  Copyright © 2015 Markus Möller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Asteroids/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Asteroids
+//
+//  Created by Markus Möller on 17/09/15.
+//  Copyright © 2015 Markus Möller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
